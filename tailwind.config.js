c0157747064d8f980023c860c8235abba1188a85
ElizabethViolin <elizabethtaylorjoy@gmail.com>
2024-06-16 00:24:39 +0800

    Initialized shadcn and updated app with modern background color

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ], 
+  theme: {
+    extend: {
+      colors: {
+        background: '#dfdfe6', 
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
